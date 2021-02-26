ba33d2dd6a01b76138b6a82f3c1796eb60a99af5
Sam Yeo <yeosamuel95@gmail.com>
2021-02-25 21:48:46 -0700

    Add link for log out and method to log out in Session controller
    Co-authored-by: V Alves de Arruda <69529052+nessaarruda@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,28 +7,23 @@ class SessionsController < ApplicationController
     token = user_info[:credentials][:token]
     
     #found_user = User.find_or_create_by(uid: uid, email: email, token: token, name: name)
-
-    found_user = User.find_or_create_by(name: name) do |user|
+    require 'pry'; binding.pry
+    found_user = User.find_or_create_by(uid: uid) do |user|
       user.uid = uid
       user.email = email
       user.token = token
       user.name = name
     end
 
-    found_user.save
     session[:uid] = uid
+    # Only send this request IF we created a user in this action
+    # If we Found a user, we will NOT send this post request
+    # post "backend.herokuapp.com/api/v1/users/#{found_user.id}"
     redirect_to dashboard_path
   end
+  
+  def destroy
+    session.delete(:uid)
+    redirect_to root_path
+  end
 end
-
-# conn = Faraday.new(
-#   url: 'https://www.googleapis.com/',
-#   headers: {'Accept': 'application/json'}
-# )
-# require 'pry'; binding.pry
-
-# response = conn.post('/login/oauth/access_token') do |req|
-#   req.params['client_id'] = client_id
-#   req.params['code'] = code
-#   req.params['client_secret'] = client_secret
-# end
