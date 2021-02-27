704d9b83353c7cce73e5ced53a76d1c3fd3dba7a
Isabelle Villasenor <villasenori@outlook.com>
2021-02-27 14:22:44 -0700

    Add Services for Excursion
    Co-authored-by: Yesi Meza <69552154+Yesi-MC@users.noreply.github.com>

---
+++ app/services/excursions_service.rb
@@ -0,0 +1,18 @@
+class ExcursionsService
+  class << self
+    def create_excursion(params_2)
+      response = conn.get("api/v1/users/#{params_2[:user_id]}/excursions/create?#{params_2.to_query}")
+      parse_data(response)
+    end
+
+    private
+
+    def conn
+      @conn ||= Faraday.new(url: "https://go-local-be.herokuapp.com/")
+    end
+
+    def parse_data(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
