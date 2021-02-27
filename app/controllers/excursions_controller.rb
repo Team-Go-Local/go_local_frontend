2a782d45439c25c8e7667ec90db7aff6742e07b5
Sam Yeo <yeosamuel95@gmail.com>
2021-02-26 18:20:06 -0700

    Add methods edit and update
    Co-authored-by: V Alves de Arruda <69529052+nessaarruda@users.noreply.github.com>

--- app/controllers/excursions_controller.rb
+++ app/controllers/excursions_controller.rb
@@ -12,4 +12,19 @@ class ExcursionsController < ApplicationController
     flash[:notice] = "You have successfully created an Excursion!"
     redirect_to dashboard_path
   end
+
+  def edit
+    
+  end
+
+  def update
+    params_2 = {
+      title: params[:title],
+      description: params[:description],
+      location: params[:location]
+    }
+    #patch "backend.herokuapp.com/users/#{current_user.id}/excursions/:excursion_id?#{params_2.to_query}"
+    flash[:notice] = "You have successfully edited an Excursion!"
+    redirect_to dashboard_path
+  end
 end
