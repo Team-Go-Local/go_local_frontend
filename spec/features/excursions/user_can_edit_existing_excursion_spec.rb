0bcdca351aed77f087593e33e76e30c2d12d1998
Yesi Meza <yesi.meza10@gmail.com>
2021-02-28 11:22:24 -0700

    created edit spec for excursions
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>

--- spec/features/excursions/user_can_edit_existing_excursion_spec.rb
+++ spec/features/excursions/user_can_edit_existing_excursion_spec.rb
@@ -3,22 +3,31 @@ require 'rails_helper'
 describe 'Excursion Edit' do
   describe 'happy path' do
     it 'can edit an excursion and be redirected to dashboard' do
+      json_response = File.read('spec/fixtures/excursion_response.json')
+      stub_request(:patch, "https://tranquil-refuge-53915.herokuapp.com/api/v1/users/123/excursions/edit?description=Sample%20Description&location=Denver,%20CO&title=Sample%20Title&user_id=123")
+        .to_return(status: 200, body: json_response)
+
+      user = instance_double('User')
+      allow(user).to receive(:name).and_return('Yesi')
+      allow(user).to receive(:id).and_return(123)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       visit root_path
       stub_omniauth
       within('.login') { click_link }
-      #create the excursion (mock call to BE, should respond with JSON)
+     
       #within the excursion block
-      click_button "Edit" #calls BE with the id to edit (stub once we have facade and service)
+      click_button "Edit" 
 
       fill_in :title, with: "Sample Title"
       fill_in :description, with: "Sample Description"
-      fill_in :location, with: "Denver, CO"
+      fill_in :location, with: "Broomfiled, CO"
 
       click_button "Update Excursion"
 
       expect(current_path).to eq(dashboard_path)
       within('#local') do
-        expect(page).to have_content("Denver, CO")
+        expect(page).to have_content("Broomfield, CO")
       end
     end
   end
