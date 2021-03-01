ce00cba510998226040c1cfbe8df350951863b72
Yesi Meza <yesi.meza10@gmail.com>
2021-02-28 17:42:10 -0700

    updated the facade
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>

--- app/facades/excursions_facade.rb
+++ app/facades/excursions_facade.rb
@@ -1,7 +1,13 @@
 class ExcursionsFacade 
   class << self
     def create_excursion(params_2)
-      ExcursionsService.create_excursion(params_2)
+      data = ExcursionsService.create_excursion(params_2)
+      # excursions = ExcrusionsDetails.new(data[:data][:attributes])
+    end
+
+    def updated_excursions(params_2)
+      data = ExcursionsService.update_excursions(params_2)
+      # excursions = ExcrusionsDetails.patch(data[:data][:attributes])
     end
   end
 end
