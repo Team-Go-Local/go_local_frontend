bb55f83e357f5906f8ffe19c7bddb1dc3e6e47f0
Yesi Meza <yesi.meza10@gmail.com>
2021-03-02 15:26:35 -0700

    created a facade spec
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

--- spec/facades/excursion_facade_spec.rb
+++ spec/facades/excursion_facade_spec.rb
@@ -20,5 +20,17 @@ RSpec.describe ExcursionsFacade do
       expect(excursion[:data][:attributes][:formatted_address]).to be_a String
       expect(excursion[:data][:attributes][:types]).to be_an Array
     end
+    describe "list all excursions" do 
+      it "can receive a list of all excursions" do 
+        json_response = File.read('spec/fixtures/all_excursions.json')
+        stub_request(:get, "https://tranquil-refuge-53915.herokuapp.com/api/v1/excursions").to_return(status: 200, body: json_response)
+  
+        json_data = ExcursionsFacade.list_all_excursions
+
+        expect(json_data).to be_an(Array)
+        expect(json_data.first).to be_an(Excursion)
+        expect(json_data.count).to eq(3)
+      end
+    end
   end
 end
