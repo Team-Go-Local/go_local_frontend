8350f74363651729efba3a4ef2cafae9ee3b09e0
Yesi Meza <yesi.meza10@gmail.com>
2021-03-02 11:48:19 -0700

    add stubbed response on test and added expectations for explore page
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

--- spec/features/explore/index_spec.rb
+++ spec/features/explore/index_spec.rb
@@ -6,19 +6,25 @@ describe 'Explore Landing Page' do
     user = create(:omniauth_mock_user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     excursion_list = build_list(:excursion, 3)
-    # Fake JSON objects representing a list of Excursions
+
     stub_request(:post, "https://tranquil-refuge-53915.herokuapp.com/api/v1/users/#{user.id}").to_return(status: 204)
 
     visit root_path
     within('.login') { click_link }
+
+    json_response = File.read('spec/fixtures/excursion_response.json')
+    stub_request(:get, "https://tranquil-refuge-53915.herokuapp.com/api/v1/excursions").to_return(status: 200, body: json_response)
+    
+    
 
     click_button "Explore"
 
     expect(current_path).to eq('/explore')
     expect(page).to have_button("Search by City")
     # within('section.excursion-list') do
-      # we need save button
-      # brief description
+    expect(page).to have_content(excursion_list.title)
+    expect(page).to have_content(excursion_list.description)
+    expect(page).to have_button("Save")
       # we need to see a list of excursions from earlier
     # end
   end
