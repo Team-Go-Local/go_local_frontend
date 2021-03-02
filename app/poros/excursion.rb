d8055f2144b6842845863c709e3f869fef9b6f55
Yesi Meza <yesi.meza10@gmail.com>
2021-03-02 11:16:11 -0700

    created a excurion poros
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

---
+++ app/poros/excursion.rb
@@ -0,0 +1,12 @@
+class Excursion
+  attr_reader :title,
+              :location,
+              :place_id,
+              :description
+  def initialize(data)
+    @title = data[:attributes][:title]
+    @location = data[:attributes][:location]
+    @place_id = data[:attributes][:place_id]
+    @description = data[:attributes][:description]
+  end
+end
