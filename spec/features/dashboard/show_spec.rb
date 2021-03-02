46dd8c5d18f433b8ce9649da0f0ad721e1223b4b
Saundra <saundracatalina@gmail.com>
2021-03-01 20:48:28 -0700

    Add test to show users excursions on dashboard
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Show' do
   describe 'happy path' do
     it 'can see a basic traveler and local boards' do
-      visit root_path
-      stub_omniauth
-      within('.login') { click_link }
+      user = create(:omniauth_mock_user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit dashboard_path
 
       within '#traveller' do
         expect(page).to have_content('Traveller')
@@ -14,8 +15,21 @@ RSpec.describe 'Show' do
       within '#local' do
         expect(page).to have_content('Local')
         expect(page).to have_button('Add Excursions')
-        expect(page).to have_button('Delete')
-        expect(page).to have_button('Edit')
+      end
+    end
+    it 'shows excursions created by the user' do
+      user = create(:omniauth_mock_user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      excursions = build_list(:excursion, 3)
+
+      visit dashboard_path
+
+      within '#my_excursions' do
+        excursions.each do |excursion|
+          expect(page).to have_content(excursion.title)
+        end
+        expect(page).to have_button('Delete', count: 3)
+        expect(page).to have_button('Edit', count: 3)
       end
     end
   end
