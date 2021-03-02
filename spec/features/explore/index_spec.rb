cabd9b85005f6786190d8075b6ebd59a88a5e1b3
Yesi Meza <yesi.meza10@gmail.com>
2021-03-02 14:52:59 -0700

    edited test to parse through json fixure data
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

--- spec/features/explore/index_spec.rb
+++ spec/features/explore/index_spec.rb
@@ -5,27 +5,24 @@ describe 'Explore Landing Page' do
     stub_omniauth
     user = create(:omniauth_mock_user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    excursion_list = build_list(:excursion, 3)
 
     stub_request(:post, "https://tranquil-refuge-53915.herokuapp.com/api/v1/users/#{user.id}").to_return(status: 204)
 
     visit root_path
     within('.login') { click_link }
 
-    json_response = File.read('spec/fixtures/excursion_response.json')
+    json_response = File.read('spec/fixtures/all_excursions.json')
     stub_request(:get, "https://tranquil-refuge-53915.herokuapp.com/api/v1/excursions").to_return(status: 200, body: json_response)
     
-    
-
     click_button "Explore"
 
     expect(current_path).to eq('/explore')
     expect(page).to have_button("Search by City")
-    # within('section.excursion-list') do
-    expect(page).to have_content(excursion_list.title)
-    expect(page).to have_content(excursion_list.description)
+
+    json_data = JSON.parse(json_response, symbolize_names: true)
+
+    expect(page).to have_content(json_data[:data].first[:attributes][:title])
+    expect(page).to have_content(json_data[:data].first[:attributes][:description])
     expect(page).to have_button("Save")
-      # we need to see a list of excursions from earlier
-    # end
   end
 end
