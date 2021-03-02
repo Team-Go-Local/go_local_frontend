f26914c5823b1a95406cd3b867172ea66f10d633
Yesi Meza <yesi.meza10@gmail.com>
2021-03-02 15:27:22 -0700

    refactored the controller into facades and services
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,11 +1,6 @@
 class ExploreController < ApplicationController
 
   def index 
-    conn = Faraday.new("https://tranquil-refuge-53915.herokuapp.com/")
-    response = conn.get("/api/v1/excursions")
-    data = JSON.parse(response.body, symbolize_names: true)
-    @excursions = data[:data].map do |excursion|
-      Excursion.new(excursion)
-    end
+    @excursions = ExcursionsFacade.list_all_excursions
   end
 end 
