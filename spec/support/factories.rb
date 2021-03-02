824847af9fc2668f31ac585fcda194721f6c9396
Sam Yeo <yeosamuel95@gmail.com>
2021-03-01 18:05:02 -0700

    Change factory default id to have more specific tests
    Co-authored-by: Yesi Meza <69552154+Yesi-MC@users.noreply.github.com>

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -4,10 +4,12 @@ FactoryBot.define do
     email { "user@example.com" }
     uid { Faker::Number.within(range: 100000..999999).to_s }
     token { Faker::Number.within(range: 100000..999999) }
+    id { 1 }
   end
   factory :omniauth_mock_user, parent: :user do
     email { 'john@example.com' }
     uid {'100000000000000000000'}
     token {'MOCK_OMNIAUTH_GOOGLE_TOKEN'}
+    id { 1 }
   end
 end
