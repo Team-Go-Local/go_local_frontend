f27cfbad1f1ca2f93f632ed842e2baa4246cd7d7
Yesi Meza <yesi.meza10@gmail.com>
2021-03-02 14:54:03 -0700

    removed comments from the explore controller
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,12 +1,11 @@
 class ExploreController < ApplicationController
 
   def index 
-    # @excursions = Faraday.get("https://tranquil-refuge-53915.herokuapp.com/api/v1/excursions")
     conn = Faraday.new("https://tranquil-refuge-53915.herokuapp.com/")
     response = conn.get("/api/v1/excursions")
     data = JSON.parse(response.body, symbolize_names: true)
-    @excursions = data.map do |excursion|
+    @excursions = data[:data].map do |excursion|
       Excursion.new(excursion)
     end
   end
-end
+end 
