ab6ddbddc6850bbe17d08d471548792aaee28793
Sam Yeo <yeosamuel95@gmail.com>
2021-03-03 15:03:17 -0700

    Merge last conflict
    Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>
    Co-authored-by: V Alves de Arruda <69529052+nessaarruda@users.noreply.github.com>
    Co-authored-by: Yesi Meza <69552154+Yesi-MC@users.noreply.github.com>

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -5,8 +5,4 @@ class ExploreController < ApplicationController
     @excursions = []
     flash[:notice] = "We're sorry, we were unable to locate the content you requested. Please try again later."
   end
-<<<<<<< HEAD
 end 
-=======
-end
->>>>>>> ab5746486c34f49c93f938abe79ec04af6b8dc8d
