7724c2826fe4ad454747dea21e6220fbade3b5e0
Yesi Meza <yesi.meza10@gmail.com>
2021-03-02 18:29:41 -0700

    updated test- added stubbed in facade
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- spec/features/explore/index_spec.rb
+++ spec/features/explore/index_spec.rb
@@ -7,6 +7,7 @@ describe 'Explore Landing Page' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     stub_request(:post, "https://tranquil-refuge-53915.herokuapp.com/api/v1/users/#{user.id}").to_return(status: 204)
+    allow(DashboardFacade).to receive(:user_excursions).and_return([])
 
     visit root_path
     within('.login') { click_link }
