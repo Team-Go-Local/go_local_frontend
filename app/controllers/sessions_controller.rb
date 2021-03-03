e714dd9740b5f5c6a39d4ba2210d68d7cd3375f5
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-03 12:04:28 -0700

    add test and error handling for failed call to BE to get all excursions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,16 @@
 class SessionsController < ApplicationController
   def create
     user_info = request.env['omniauth.auth']
+    name = user_info[:info][:name]
+    uid = user_info[:uid]
+    email = user_info[:info][:email]
+    token = user_info[:credentials][:token]
 
-    user = User.find_or_create_by(uid: user_info[:uid]) do |new_user|
-      new_user.uid = user_info[:uid]
-      new_user.email = user_info[:info][:email]
-      new_user.token = user_info[:credentials][:token]
-      new_user.name = user_info[:info][:name]
+    user = User.find_or_create_by(uid: uid) do |new_user|
+      new_user.uid = uid
+      new_user.email = email
+      new_user.token = token
+      new_user.name = name
       Faraday.post("https://tranquil-refuge-53915.herokuapp.com/api/v1/users/#{new_user.id}")
     end
 
