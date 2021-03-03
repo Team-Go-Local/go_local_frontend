2fd5d5adf9ea2ae961356fa52b16b6f5bfb49b08
Isabelle Villasenor <villasenori@outlook.com>
2021-03-03 14:07:48 -0700

    Refactor User Create Method
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,6 @@ class SessionsController < ApplicationController
     email = user_info[:info][:email]
     token = user_info[:credentials][:token]
 
-    # user = User.find_or_create_by(uid: uid) do |user|
-    #   user.uid = uid
-    #   user.email = email
-    #   user.token = token
-    #   user.name = name
-    #   Faraday.post("https://tranquil-refuge-53915.herokuapp.com/api/v1/users/#{user.id}")
-    # end
     if User.exists?(uid: uid)
       user = User.find_by(uid: uid)
     else
