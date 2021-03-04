acdfc1e5165f31ed6cfbfd8bb49f3d152f468764
Isabelle Villasenor <villasenori@outlook.com>
2021-03-03 21:26:57 -0700

    Add city_list Facade Call into Explore Controller
    Co-authored-by: Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,8 +1,10 @@
 class ExploreController < ApplicationController
   def index
     @excursions = ExcursionsFacade.list_all_excursions
+    @cities = ExcursionsFacade.city_list
   rescue JSON::ParserError
     @excursions = []
+    @cities = []
     flash[:notice] = "We're sorry, we were unable to locate the content you requested. Please try again later."
   end
 end 
